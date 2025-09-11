ee17bb34d3847a089ec9824eb4dc57789a91ede9
tsato1 <9661884+tsato1@users.noreply.github.com>
2025-09-11 17:48:37 +0900

    feat: auto navigation to home

--- src/components/error-component.tsx
+++ src/components/error-component.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { signOut } from "aws-amplify/auth";
+import { HomeIcon, TriangleAlert } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+
 interface ErrorPageProps {
   title?: string
   message?: string
@@ -5,12 +21,53 @@ interface ErrorPageProps {
 
 export const ErrorComponent = ({
   title = "エラー",
-  message = "ユーザデータが取得できませんでした。"
+  message = "ユーザデータが取得できませんでした。ログインし直してください。"
 }: ErrorPageProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const doSignOut = async () => {
+    setIsLoading(true);
+
+    await signOut();
+    window.location.href = "/";
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      doSignOut();
+      return () => clearTimeout(timer);
+    }, 10 * 1000); // 10 seconds
+  }, [])
+
   return (
-    <div className="w-full h-full flex items-center justify-center mt-10">
-      <h1>{title}</h1>
-      <p>{message}</p>
-    </div>
+    <Card className="w-full h-full mx-auto mt-10 flex flex-col items-center text-center">
+      <CardHeader className="w-[60%] flex flex-col items-center justify-center rounded-md p-2 sm:p-4 bg-red-50">
+        <CardTitle className="flex items-center text-2xl whitespace-nowrap text-destructive">
+          <TriangleAlert className="mr-1 sm:mr-2" />
+          <h1>{title}</h1>
+        </CardTitle>
+        <CardDescription>
+          {message}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>10秒後にホーム画面へ戻ります。</p>
+        <p>自動で遷移しない場合は下のボタンをクリックしてください。</p>
+      </CardContent>
+      <CardFooter>
+        <Button
+          variant="outline"
+          onClick={() => doSignOut()}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <div className="size-5 mr-1 animate-spin bg-red-400" />
+          ) : (
+            <HomeIcon className="size-5 mr-1" />
+          )}
+          ホーム画面へ
+        </Button>
+      </CardFooter>
+    </Card>
   )
 }
