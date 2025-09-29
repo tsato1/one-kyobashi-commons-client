ca671e55e39797397ecbecdb011edd90d6534b95
tsato1 <9661884+tsato1@users.noreply.github.com>
2025-09-29 16:38:05 +0900

    feat: welcome page

---
+++ src/app/[locale]/(dashboard)/(guests)/welcome/_components/navbar-welcome.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image"
+
+export const NavbarWelcome = () => {
+  return (
+    <div className="fixed flex items-center justify-end w-screen h-16 sm:h-20 top-0 z-50">
+      <Link
+        href="/"
+        className="relative w-12 sm:w-16 h-12 sm:h-16 mr-auto cursor-pointer"
+      >
+        <Image
+          src="/logo.png"
+          alt="logo"
+          sizes="(max-width: 600px) 100vw, (max-width: 780px) 50vw, 33vw"
+          style={{ objectFit: "cover" }}
+          priority
+          fill />
+      </Link>
+
+      <div className="hidden md:block">
+        {/* <LanguageSelect locale={locale} /> */}
+      </div>
+    </div>
+  )
+}
--- src/app/[locale]/(dashboard)/welcome/page.tsx
+++
@@ -1,7 +0,0 @@
-const WelcomePage = () => {
-  return (
-    <>Welcome page!!!</>
-  )
-}
-
-export default WelcomePage
