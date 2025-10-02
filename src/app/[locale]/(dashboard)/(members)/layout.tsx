80573e6dc4fcdf65a79135807335282f969eb621
tsato1 <9661884+tsato1@users.noreply.github.com>
2025-10-03 01:36:50 +0900

    feat: app sidebar

---
+++ src/app/[locale]/(dashboard)/(members)/layout.tsx
@@ -0,0 +1,29 @@
+
+import { ErrorComponent } from "@/components/error-component";
+import { sidebarDataTrustee, sidebarDataCrew } from "@/constants/navbar-data";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { useGetAuthUserQuery } from "@/state/api";
+import { AppSidebar } from "./_components/app-sidebar";
+
+interface MembersLayout {
+  children: React.ReactNode;
+}
+
+const MembersLayout = ({
+  children
+}: MembersLayout) => {
+  const { data: authUser } = useGetAuthUserQuery();
+
+  if (!authUser) {
+    return <ErrorComponent />
+  }
+
+  return (
+    <SidebarProvider>
+      <AppSidebar sidebarData={authUser.userRole === "trustee" ? sidebarDataTrustee : sidebarDataCrew} />
+      <>{children}</>
+    </SidebarProvider>
+  )
+}
+
+export default MembersLayout;
